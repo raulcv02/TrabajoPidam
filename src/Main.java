93bb3fe49bd4f862d4ab098f1edfa0cffd494ad5
ferra <ferranlatorre6@gmail.com>
2025-09-19 13:58:14 +0200

    este es el de comprobar si existe el archivo y falta el case

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,134 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    static void eliminarArchivo(File archivo) throws FileNotFoundException{
+        if(!archivo.exists()){
+            System.out.println("El archivo no existe");
+            throw new IllegalArgumentException("El archivo no existe");
+        } else  if(archivo.isFile()){
+            archivo.delete();
+            System.out.println("El archivo ha sido eliminado corréctamente");
+        } else  if(archivo.isDirectory()){
+            File[] archivos = archivo.listFiles();
+            for(File f : archivo.listFiles()){
+                eliminarArchivo(f);
+            }
         }
     }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("######MENU######");
+
+        System.out.println("1 Crear Archivo");
+        System.out.println("2 Ejemplo");
+
+        System.out.println("3 Eliminar");
+
+
+        System.out.print("\nOpcion: ");
+
+        // Esto hace que si lo que han escrito en el scanner no es un numero que se quede atascado ahi
+        while (!sc.hasNextInt()) {
+
+            System.out.println("Introduce un numero no letras");
+            sc.nextLine();
+
+            System.out.print("\nOpcion: ");
+        }
+
+        int opt = sc.nextInt();
+        sc.nextLine();
+
+
+        switch (opt) {
+
+            case 1: {
+
+                crearArchivo();
+                break;
+            }
+
+            case 2: {
+                //listar directorio
+                System.out.println("Introduce la ruta del directorio: ");
+                String rutaDirectorio = sc.nextLine();
+
+                File directorio = new File(rutaDirectorio);
+
+                if (directorio.exists() && directorio.isDirectory()) {
+                    String[] archivos = directorio.list();
+
+                    System.out.println("\nContenido del directorio: ");
+                    for (String archivo : archivos) {
+                        System.out.println(archivo);
+                    }
+                } else {
+                    System.out.println("La ruta no es un directorio válido");
+                }
+                break;
+            }
+
+            case 3: {
+                System.out.print("Archivo: ");
+                File file = new File(sc.nextLine());
+                eliminarArchivo(file);
+                break;
+            }
+
+            default: {
+                System.out.println("Opción no valida.");
+                break;
+            }
+        }
+    }
+
+
+    public static void crearArchivo() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introduce el nombre del archivo: ");
+        File file = new File(sc.nextLine());
+        try {
+
+            if (file.createNewFile()) {
+
+                System.out.println("Archivo creado correctamente");
+            }
+            else {
+
+                System.out.println("Error al crear el archivo, el archivo ya existia");
+            }
+        }
+        catch (IOException e) {
+
+            System.out.println("Error al crear el archivo");
+            throw new RuntimeException(e);
+
+        }
+    }
+
+    public static void comprobarArchivo() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introduce la ruta del archivo a comprobar: ");
+        File archivo = new File(sc.nextLine());
+
+        if (archivo.exists()) {
+            if (archivo.isFile()) {
+                System.out.println(" El archivo existe: " + archivo.getAbsolutePath());
+            } else if (archivo.isDirectory()) {
+                System.out.println(" Es un directorio: " + archivo.getAbsolutePath());
+            }
+        } else {
+            System.out.println(" El archivo no existe.");
+        }
+    }
+
+
 }
